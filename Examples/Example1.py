03d9dfa6276b05bdb77eb9d47772d592c735fe3a
martin <martindelosrios13@gmail.com>
2021-01-02 16:02:24 -0300

    I change to random cosmologies in example 1

--- Examples/Example1.py
+++ Examples/Example1.py
@@ -7,12 +7,12 @@ import numpy as np
 
 pars = camb.read_ini('/home/martin/Descargas/CAMB/inifiles/planck_2018.ini')
 
-H0_true  = 67.32117
-omb_true = 0.0223828
-omc_true = 0.1201075
-n_true   = 0.9660499
-tau_true = 0.05430842
-As_true  = 2.100549e-9
+H0_true  = np.random.normal(67.32117, 5)
+omb_true = np.random.normal(0.0223828, 0.001)
+omc_true = np.random.normal(0.1201075, 0.05)
+n_true   = np.random.normal(0.9660499, 0.01)
+tau_true = np.random.normal(0.05430842, 0.001)
+As_true  = np.random.normal(2.100549e-9, 0.1e-9)
 
 #calculate results for these parameters
 pars.set_cosmology(H0 = H0_true, ombh2 = omb_true, omch2 = omc_true, tau = tau_true)
