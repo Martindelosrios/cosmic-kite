a3c4b419d98b4a5cb602963124c185c5996f41ec
martindelosrios <martindelosrios13@gmail.com>
2021-01-27 11:05:47 -0300

    I added all the models}

--- src/cosmic_kite.py
+++ src/cosmic_kite.py
@@ -2,15 +2,19 @@ import os
 import pkg_resources
 import numpy as np
 
-location = os.path.dirname(os.path.realpath(__file__))
+location  = os.path.dirname(os.path.realpath(__file__))
 data_path = os.path.join(location, 'data')
 
 # Let's load the scaler
 from pickle import load
 from sklearn import preprocessing
 
-scaler_x = load(open(data_path + '/scaler_x.pkl', 'rb'))
-scaler_y = load(open(data_path + '/scaler_y.pkl', 'rb'))
+scaler_x_TT = load(open(data_path + '/scaler_x_TT.pkl', 'rb'))
+scaler_y_TT = load(open(data_path + '/scaler_y_TT.pkl', 'rb'))
+scaler_x_EE = load(open(data_path + '/scaler_x_EE.pkl', 'rb'))
+scaler_y_EE = load(open(data_path + '/scaler_y_EE.pkl', 'rb'))
+scaler_x_TE = load(open(data_path + '/scaler_x_TE.pkl', 'rb'))
+scaler_y_TE = load(open(data_path + '/scaler_y_TE.pkl', 'rb'))
 
 # Let's load some neccesary libraries
 from tensorflow.keras.models import Model
@@ -19,9 +23,9 @@ from tensorflow.keras.layers import Input, Dense, LeakyReLU
 # Let's define the architecture and load the trained values
 
 # Armo los tensores input
-lmin = 2
-lmax = 2650
-actFun = 'LeakyReLU'
+lmin              = 2
+lmax              = 2650
+actFun            = 'LeakyReLU'
 batch_size        = 256
 original_dim      = 1*(lmax-lmin)
 latent_dim        = 6
@@ -31,9 +35,10 @@ intermediate_dim3 = 100
 intermediate_dim4 = 50
 epochs            = 100
 epsilon_std       = 1.0
-input_shape = (original_dim,)
+input_shape       = (original_dim,)
 
-# VAE model = encoder + decoder
+# TT VAE model = encoder + decoder
+#{{{
 # build encoder model
 inputs    = Input(shape=input_shape, name='encoder_input')
 x         = Dense(intermediate_dim)(inputs)
@@ -45,7 +50,7 @@ x         = LeakyReLU(alpha=0.1)(x)
 latent    = Dense(latent_dim, name='z_mean', activation='linear')(x)
 
 # instantiate encoder model
-encoder = Model(inputs, latent, name='encoder')
+encoder_TT = Model(inputs, latent, name='encoder')
 
 # build decoder model
 latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
@@ -58,14 +63,85 @@ x             = LeakyReLU(alpha=0.1)(x)
 outputs       = Dense(original_dim, activation='sigmoid')(x)
 
 # instantiate decoder model
-decoder = Model(latent_inputs, outputs, name='decoder')
+decoder_TT = Model(latent_inputs, outputs, name='decoder')
 
 # instantiate VAE model
-dec_output = decoder(latent)
-enc_output = encoder(inputs)
-vae        = Model(inputs, [enc_output, dec_output], name='vae_mlp')
+dec_output = decoder_TT(latent)
+enc_output = encoder_TT(inputs)
+vae_TT        = Model(inputs, [enc_output, dec_output], name='vae_mlp')
 
-vae.load_weights(data_path + '/vae_model.h5')
+vae_TT.load_weights(data_path + '/vae_model_TT.h5')
+#}}}
+# EE VAE model = encoder + decoder
+#{{{
+# build encoder model
+inputs    = Input(shape=input_shape, name='encoder_input')
+x         = Dense(intermediate_dim)(inputs)
+x         = LeakyReLU(alpha=0.1)(x)
+x         = Dense(intermediate_dim3)(x)
+x         = LeakyReLU(alpha=0.1)(x)
+x         = Dense(intermediate_dim4)(x)
+x         = LeakyReLU(alpha=0.1)(x)
+latent    = Dense(latent_dim, name='z_mean', activation='linear')(x)
+
+# instantiate encoder model
+encoder_EE = Model(inputs, latent, name='encoder')
+
+# build decoder model
+latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+x             = Dense(intermediate_dim4)(latent_inputs)
+x             = LeakyReLU(alpha=0.1)(x)
+x             = Dense(intermediate_dim3)(x)
+x             = LeakyReLU(alpha=0.1)(x)
+x             = Dense(intermediate_dim)(x)
+x             = LeakyReLU(alpha=0.1)(x)
+outputs       = Dense(original_dim, activation='sigmoid')(x)
+
+# instantiate decoder model
+decoder_EE = Model(latent_inputs, outputs, name='decoder')
+
+# instantiate VAE model
+dec_output = decoder_EE(latent)
+enc_output = encoder_EE(inputs)
+vae_EE        = Model(inputs, [enc_output, dec_output], name='vae_mlp')
+
+vae_EE.load_weights(data_path + '/vae_model_EE.h5')
+#}}}
+# TE VAE model = encoder + decoder
+#{{{
+# build encoder model
+inputs    = Input(shape=input_shape, name='encoder_input')
+x         = Dense(intermediate_dim)(inputs)
+x         = LeakyReLU(alpha=0.1)(x)
+x         = Dense(intermediate_dim3)(x)
+x         = LeakyReLU(alpha=0.1)(x)
+x         = Dense(intermediate_dim4)(x)
+x         = LeakyReLU(alpha=0.1)(x)
+latent    = Dense(latent_dim, name='z_mean', activation='linear')(x)
+
+# instantiate encoder model
+encoder_TE = Model(inputs, latent, name='encoder')
+
+# build decoder model
+latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+x             = Dense(intermediate_dim4)(latent_inputs)
+x             = LeakyReLU(alpha=0.1)(x)
+x             = Dense(intermediate_dim3)(x)
+x             = LeakyReLU(alpha=0.1)(x)
+x             = Dense(intermediate_dim)(x)
+x             = LeakyReLU(alpha=0.1)(x)
+outputs       = Dense(original_dim, activation='sigmoid')(x)
+
+# instantiate decoder model
+decoder_TE = Model(latent_inputs, outputs, name='decoder')
+
+# instantiate VAE model
+dec_output = decoder_TE(latent)
+enc_output = encoder_TE(inputs)
+vae_TE        = Model(inputs, [enc_output, dec_output], name='vae_mlp')
+
+vae_TE.load_weights(data_path + '/vae_model_TE.h5')
+#}}}
 
 # Let's define de main functions
 
@@ -76,24 +152,32 @@ def pars2ps(pars):
   if ((np.max(pars[:,3]) > 0.99454) or (np.min(pars[:,3]) < 0.937550)): print('WARNING! Some values of n are outside the training range')
   if ((np.max(pars[:,4]) > 0.094) or (np.min(pars[:,4]) < 0.01430)): print('WARNING! Some values of tau are outside the training range')
   if ((np.max(pars[:,5]) > 2.2705e-9) or (np.min(pars[:,5]) < 1.9305e-9)): print('WARNING! Some values of As are outside the training range')
-  pars_scaled    = scaler_y.transform(pars)
-  ps_pred_scaled = decoder.predict(pars_scaled)
-  ps_pred        = scaler_x.inverse_transform(ps_pred_scaled)
-  tt_pred        = ps_pred[:,0:(lmax-lmin)]
+  pars_scaled    = scaler_y_TT.transform(pars)
   ell_array      = np.arange(lmin, lmax)
-  return [tt_pred, ell_array]
+  tt_pred_scaled = decoder_TT.predict(pars_scaled)
+  tt_pred        = scaler_x_TT.inverse_transform(tt_pred_scaled)[:,0:(lmax-lmin)]
+  ee_pred_scaled = decoder_EE.predict(pars_scaled)
+  ee_pred        = scaler_x_EE.inverse_transform(ee_pred_scaled)[:,0:(lmax-lmin)]
+  te_pred_scaled = decoder_TE.predict(pars_scaled)
+  te_pred        = scaler_x_TE.inverse_transform(te_pred_scaled)[:,0:(lmax-lmin)]
+  return [tt_pred, ee_pred, te_pred, ell_array]
 
 
-def ps2pars(ps):
-  ps_scaled        = scaler_x.transform(ps)
-  pars_pred_scaled = encoder.predict(ps_scaled)
-  pars_pred        = scaler_y.inverse_transform(pars_pred_scaled)
+def tt2pars(ps):
+  ps_scaled        = scaler_x_TT.transform(ps)
+  pars_pred_scaled = encoder_TT.predict(ps_scaled)
+  pars_pred        = scaler_y_TT.inverse_transform(pars_pred_scaled)
   return pars_pred  
-#def pars2ps(H0, ombh2, omch2, n, tau, As):
-#  params  = scaler_y.transform([(ombh2, omch2, H0, n, tau, As)])
-#  pred_cl = scaler_x.inverse_transform(decoder.predict(params))[0,:]
-#  return pred_cl
-#
-#def ps2pars(ps):
-#  pars = scaler_y.inverse_transform(encoder.predict(scaler_x.transform(ps.reshape(1,2450)))[0][0:6])
-#  return pars
+
+def ee2pars(ps):
+  ps_scaled        = scaler_x_EE.transform(ps)
+  pars_pred_scaled = encoder_EE.predict(ps_scaled)
+  pars_pred        = scaler_y_EE.inverse_transform(pars_pred_scaled)
+  return pars_pred  
+
+def te2pars(ps):
+  ps_scaled        = scaler_x_TE.transform(ps)
+  pars_pred_scaled = encoder_TE.predict(ps_scaled)
+  pars_pred        = scaler_y_TE.inverse_transform(pars_pred_scaled)
+  return pars_pred  
+
